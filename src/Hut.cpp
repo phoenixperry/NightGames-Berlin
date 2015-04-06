47015a2e65a65ae5e6be906978919fa72b6d0474
Phoenix Perry <contact@phoenixperry.com>
2015-04-06 09:11:44 +0200

    morning commit

---
+++ src/Hut.cpp
@@ -0,0 +1,9 @@
+//
+//  Hut.cpp
+//  http_example
+//
+//  Created by Phoenix Perry on 4/4/15.
+//
+//
+
+#include "Hut.h"
---
+++ src/Hut.h
@@ -0,0 +1,14 @@
+//
+//  Hut.h
+//  http_example
+//
+//  Created by Phoenix Perry on 4/4/15.
+//
+//
+
+#ifndef __http_example__Hut__
+#define __http_example__Hut__
+
+#include <stdio.h>
+
+#endif /* defined(__http_example__Hut__) */
