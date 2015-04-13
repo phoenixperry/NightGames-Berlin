bc3190f87cbadd8ab136062d8b7c38656403c757
Phoenix Perry <contact@phoenixperry.com>
2015-04-13 07:46:47 +0200

    playing with filters for stars

--- src/Stars.cpp
+++ src/Stars.cpp
@@ -17,9 +17,10 @@ Stars::Stars(Spark_core_manager *spark_){
     
     for (int i=0; i<NUM_LIGHT_SENSORS/2; i++) {
         ofxAudioUnitTap tap;
-        ofxAudioUnit varispeed;
-        varispeed.setup(kAudioUnitType_FormatConverter, kAudioUnitSubType_Varispeed);
-        filters[i] = varispeed;
+        ofxAudioUnit lowPass;
+        lowPass.setup(kAudioUnitType_Effect, kAudioUnitSubType_LowPassFilter);
+        lowPass.loadCustomPreset("LowPass_stars.aupreset");
+        filters[i] = lowPass;
         taps[i] = tap;
     }
     
@@ -36,7 +37,7 @@ Stars::Stars(Spark_core_manager *spark_){
         ofSetVerticalSync(true);
     mixer.connectTo(output);
     mixer.setInputVolume(1.0f, 2);
-    mixer.setOutputVolume(0.1f);
+    mixer.setOutputVolume(0.5f);
     output.start(); 
 }
 
@@ -68,6 +69,21 @@ void Stars::update(){
     
     float star_one_average = sensors[0] + sensors[1]/2;
     float star_two_average = sensors[2] + sensors[3]/2;
+    float newCutoff = ofMap(star_one_average, 1000, 4000, 10, 6900);
+    float newCutoffTwo = ofMap(star_two_average, 2000, 4000, 10, 6900);
+    AudioUnitSetParameter(filters[0],
+                          kLowPassParam_CutoffFrequency,
+                          kAudioUnitScope_Global,
+                          0,
+                          newCutoff,
+                          0);
+    AudioUnitSetParameter(filters[1],
+                          kLowPassParam_CutoffFrequency,
+                          kAudioUnitScope_Global,
+                          0,
+                          newCutoffTwo,
+                          0);
+
     
 }
 
