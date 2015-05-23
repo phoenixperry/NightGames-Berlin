a7f57e8d2d5b87f67d553168d84d7bf452a2fe80
Phoenix Perry <contact@phoenixperry.com>
2015-05-23 14:20:32 +0200

    before integrating OSC

--- hut_bare/Proximity_MP3/Proximity_MP3.ino
+++ hut_bare/Proximity_MP3/Proximity_MP3.ino
@@ -45,14 +45,16 @@ void writeSerialData()
     MPR121.updateTouchData();
     for(int i=0; i<12; i++){
     MPR121.updateFilteredData(); 
-    int d = MPR121.getFilteredData(i);
-      if(i < 10){
-        Serial.print(0);
-      }
-        Serial.print(i);
-        Serial.print(":");
-        Serial.println(d);
-        Serial.flush();  
+    int d = MPR121.getFilteredData(0);
+      // if(i < 10){
+      //   Serial.print(0);
+      // }
+      //   Serial.print(i);
+      //   Serial.print(":");
+      //   Serial.println(d);
+      //   Serial.flush();  
+      Serial.println(d); 
+
     if(d < 700){
       digitalWrite(leds[i], HIGH);
     }
